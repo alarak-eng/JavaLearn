b483c481bf1924b9381ae4acdb5af4cb4579ca46
alarak-eng <60296189+alarak-eng@users.noreply.github.com>
2021-09-08 17:40:56 +0800

    Add files via upload

---
+++ TheRandom.java
@@ -0,0 +1,10 @@
+
+public class TheRandom {
+public static void main(String[]args){
+        int[] num=new int[20];
+        for(int n;n<20;n++){
+            num[n]=(int)(Math.Random()*100+1);
+        }
+        
+    }
+}
